18fc92d1ba7f643acd09c17573c9a4fe9cfb62c0
David <DKrakauer18@brophybroncos.org>
2016-02-12 13:11:04 -0700

    Mostly UI

--- Roadtrip/ProfileViewController.swift
+++ Roadtrip/ProfileViewController.swift
@@ -2,24 +2,20 @@
 //  ProfileViewController.swift
 //  Roadtrip
 //
-//  Created by Robert Krakauer on 6/25/15.
-//  Copyright (c) 2015 David Krakauer. All rights reserved.
+//  Created by David Krakauer on 2/12/16.
+//  Copyright © 2016 David Krakauer. All rights reserved.
 //
-
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentVIew: UIView!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        scrollView.contentSize.height = 700
+        
+    }
 }
